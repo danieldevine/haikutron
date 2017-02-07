700c44de4e43b607e3b9f68ac1bd35d6e3537a0c
danieldevine <dan@maverick-intl.com>
2017-02-07 23:51:24 +0000

    builds bot

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 h*a*i*k*u*t*r*o*n*
 ------------------
+
+Haikutron writes nice Haiku like:
+
+#### newtonian footed
+
+social faced chemist
+
+positive art gravitons
+
+multi past research
+
+uses the datamuse API
---
+++ dist/assets/js/production.js
@@ -0,0 +1,18 @@
+/*! haikutron - v0.0.1 - 2017-02-07 */(function($) {
+    'use strict';
+
+    $(document).ready(function() {
+
+        var
+            $body = $('body'),
+            $window = $(window);
+
+
+
+    }); // end DOM Ready
+
+
+
+
+
+})(jQuery);
