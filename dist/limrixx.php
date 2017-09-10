2eee792ee174077aaa9197ef947e625abc8e7d63
Dan Devine <dan@maverick-intl.com>
2017-09-10 21:38:13 +0100

    remove head

--- dist/limrixx.php
+++ dist/limrixx.php
@@ -13,18 +13,9 @@
 
 $title = "Limerick | Limericks ";
 $description = "Limrixx is going to tweet some limericks and you can't stop them. Made in Limerick.";
-require 'layout/head.php';
+
 require 'inc/limerick.php';
 require_once 'inc/functions.php';
-?>
-<body>
-    <section class="haiku" style="background-color:<?php echo randColor(); ?>; color:<?php echo randColor(); ?>;">
-        <div class="wrapper">
-             <p><?php echo $limerick; ?></p>
-             <div class="signed">-@limrixx '<?php echo date('y') ?> </div>
-        </div>
-    </section>
-<?php
 require 'inc/tweet.php';
 require 'layout/footer.php';
 ?>
