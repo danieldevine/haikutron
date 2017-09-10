f4a67a17a7a39e9e9da81ebe82d95acfd208e8c5
Dan Devine <dan@maverick-intl.com>
2017-09-10 22:42:53 +0100

    dploy

--- dist/limrixx.php
+++ dist/limrixx.php
@@ -15,6 +15,7 @@ $title = "Limerick | Limericks ";
 $description = "Limrixx is going to tweet some limericks and you can't stop them. Made in Limerick.";
 require 'layout/head.php';
 require 'inc/limerick.php';
+require_once 'inc/functions.php';
 ?>
 <body>
     <section class="haiku" style="background-color:<?php echo randColor(); ?>; color:<?php echo randColor(); ?>;">
