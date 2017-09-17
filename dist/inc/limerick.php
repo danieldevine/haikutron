09c06d793b607460b03d057fa72bdc8415b7a835
Dan Devine <dan@maverick-intl.com>
2017-09-17 09:47:18 +0100

    whurt

--- dist/inc/limerick.php
+++ dist/inc/limerick.php
@@ -35,7 +35,7 @@ $intro = array(
  */
 $shuffled      = shuffle($intro);
 
-$theme         = "femdom";
+$theme         = "trample";
 
 $lineOne       = new Limrixx("rel_spc=man", 2, "n");
 $oneOne        = $lineOne->lxWord();
